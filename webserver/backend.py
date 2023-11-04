c9db36181b64ff1812697916393cf884449d7497
OL <oscarlay789@gmail.com>
2023-11-04 18:07:28 -0500

    updated backend to get get

--- webserver/backend.py
+++ webserver/backend.py
@@ -1,5 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
+
+@app.route('/search')
+def search():
+    inp = str(request.args.get('s'))
+
+    return return_info("yeet")
+
+def return_info(stock):
+    return stock + " still in progress"
+
+if __name__ == '__main__':
+    # run app in debug mode on port 5000
+    app.run(debug=True, port=5000)
