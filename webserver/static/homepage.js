9654e0316099896885f55ecb022b15f690232e41
Ari S <ari@Aris-MacBook-Pro.local>
2023-11-05 02:23:51 -0600

    Gauge chart

--- webserver/static/homepage.js
+++ webserver/static/homepage.js
@@ -1,6 +1,55 @@
 
 console.log("script loaded");
 
+const ctx = document.getElementById('myChart');
+var data = {
+            value: 100,
+            max: 100,
+            label: "Sentiment"
+        };
+
+        // Chart.js chart's configuration
+        // We are using a Doughnut type chart to 
+        // get a Gauge format chart 
+        // This is approach is fine and actually flexible
+        // to get beautiful Gauge charts out of it
+        var config = {
+            type: 'doughnut',
+            data: {
+                labels: [data.label],
+                datasets: [{
+                    data: [data.value, data.max - data.value],
+                    backgroundColor: [ data.value>=50 ?'#c3f0ca': '#f25042', '#0f0e17'],
+                    borderWidth: 0
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                cutoutPercentage: 85,
+                rotation: -90,
+                circumference: 180,
+                tooltips: {
+                    enabled: false
+                },
+                legend: {
+                    display: false
+                },
+                animation: {
+                    animateRotate: true,
+                    animateScale: false
+                },
+                title: {
+                    display: true,
+                    text: data.label,
+                    fontSize: 16
+                }
+            }
+        };
+
+        // Create the chart
+        var gaugeChart = new Chart(ctx, config);
+
 
 // Select the form element
 const form = document.getElementById('search');
@@ -13,7 +62,7 @@ form.addEventListener('submit', function (event) {
 
   // Create FormData object from the form
   const formData = new FormData(form);
-
+  console.log(formData);
   // Use Fetch API to send the POST request with FormData
   fetch(`/search`, {
     method: 'POST',
@@ -31,7 +80,8 @@ form.addEventListener('submit', function (event) {
     console.log('Success:', data);
     document.getElementById("main-content").classList.remove('mt-40');
     document.getElementById("blurb").classList.add('scale-0');
-    blurb.style.marginBottom = `-${blurb.offsetHeight}px`;
+    document.getElementById("blurb").style.marginBottom = `-${blurb.offsetHeight}px`;
+    document.getElementById("myChart").classList.remove('hidden');
     
     setTimeout(()=>{
       document.getElementById("blurb").classList.add("hidden");
