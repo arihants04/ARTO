4f9073e45dbcad36ecd66d7ecf19bedf16df5f2d
Ari S <ari@Aris-MacBook-Pro.local>
2023-11-04 22:09:01 -0500

    post request from frontend

--- webserver/static/homepage.js
+++ webserver/static/homepage.js
@@ -1,34 +1,36 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//   // Get the form by its ID
-//   var searchForm = document.getElementById('search');
 
-//   // Add a submit event listener to the form
-//   searchForm.addEventListener('submit', function(event) {
-//     print();
-//     // Prevent the default form submission
-//     event.preventDefault();   
+console.log("script loaded");
 
-//     // Get the search query from the form input with ID 'search'
-//     var searchQuery = document.getElementById("default-search").value;
 
-//     // Change the current page location to include the query
-//     document.location.href = `/search?s=${encodeURIComponent(searchQuery)}`;
+// Select the form element
+const form = document.getElementById('search');
+const url= window.location.href;
 
-//     // // Send a POST request to the server
-//     // fetch('/search', {
-//     //   method: 'POST',
-//     //   headers: {
-//     //     'Content-Type': 'application/x-www-form-urlencoded',
-//     //   },
-//     //   body: `q=${encodeURIComponent(searchQuery)}`,
-//     // })
-//     // .then(response => response.json())
-//     // .then(data => {
-//     //   console.log('Success:', data);
-//     //   // Handle the response data
-//     // })
-//     // .catch((error) => {
-//     //   console.error('Error:', error);
-//     // });
-//   });
-// });
+// When the form is submitted
+form.addEventListener('submit', function (event) {
+  // Prevent the default form submit
+  event.preventDefault();
+
+  // Create FormData object from the form
+  const formData = new FormData(form);
+
+  // Use Fetch API to send the POST request with FormData
+  fetch(`/${url}/search`, {
+    method: 'POST',
+    body: formData // FormData is automatically set as the body
+    // No need to set 'Content-Type': 'multipart/form-data' header,
+    // since the browser sets it automatically with the proper boundary
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok: ' + response.statusText);
+    }
+    return response.text(); // or response.json() if your server responds with JSON
+  })
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+});
